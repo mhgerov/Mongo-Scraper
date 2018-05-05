7d0d1a37f3e1e736bb19159b62d6186ec789c38c
Michael Gerov <mhgerov@gmail.com>
2018-05-04 23:52:49 -0400

    can push to db (need to enforce unique)

--- controller/index.js
+++ controller/index.js
@@ -1,9 +1,42 @@
 var models = require('../models');
+var request = require('request');
+var cheerio = require('cheerio');
 module.exports = {}
 
-exports.getHeadlines = function (query) {
+module.exports.getHeadlines = function (query) {
 }
 
-exports.scrape = function () {
-
+module.exports.scrape = function (cb) {
+	request('https://www.nytimes.com/section/us', (err, res, body) => {
+		if (err) console.log('Request Error: '+err);
+		var $ = cheerio.load(body);
+		var articles = [];
+		var count = 0;
+		$('ol.initial-set').each(function() {
+			//').each(function() {
+			if (count<1) {
+				$(this).find('li[id^="story"]').each(function() {
+					var article = {};
+					//url
+					article.url = $(this).find('a').attr('href'); 
+					//headline
+					article.headline = $(this).find('.headline').text().trim();
+					//summary
+					article.summary = $(this).find('.summary').text().trim();
+					//date
+					article.date = $(this).find('time').attr('content');
+					//Add to articles array
+					articles.push(article);
+				});
+				count++;
+			}
+		});
+		//console.log(articles);
+		console.log('Number articles scraped: '+articles.length);
+		models.Article.create(articles, function (err, objs) {	
+			//Callback function
+			cb();
+		});
+	});
 }
+
